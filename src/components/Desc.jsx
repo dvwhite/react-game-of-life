771200058aaadf42e3caf7e07316a84d7068227a
David White <dwhite0627@gmail.com>
2020-07-23 22:54:22 -0400

    Add the Desc component to print a textual description of game rules

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
+// Styled component imports
+import styled from "styled-components";
+
+const TextBox = styled.div`
+  border: 1px solid black;
+  width: 40%;
+  margin-top: 2%;
+  margin-right: 10%;
+  display: flex;
+  align-self: flex-start;
+  flex-direction: column;
+`;
+
+const TextArea = styled.div`
+  text-align: left;
+
+  h2 {
+    text-align: center;
+  }
+
+  p {
+    margin: 2%;
+    padding: 1%;
+  }
+`;
+
 const Desc = () => {
-  return <p>.</p>;
+  return (
+    <TextBox>
+      <TextArea>
+        <h2>The Rules</h2>
+        <p>
+          1. Any live cell with fewer than two live neighbours dies, as if by
+          underpopulation.
+        </p>
+        <p>
+          2. Any live cell with two or three live neighbours lives on to the
+          next generation.
+        </p>
+        <p>
+          3. Any live cell with more than three live neighbours dies, as if by
+          overpopulation.
+        </p>
+        <p>
+          4. Any dead cell with exactly three live neighbours becomes a live
+          cell, as if by reproduction.
+        </p>
+      </TextArea>
+    </TextBox>
+  );
 };
 
 export default Desc;
