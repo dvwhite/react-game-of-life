85ba598a574dacbf032c112743c779b872aba77e
David White <dwhite0627@gmail.com>
2020-07-23 22:53:24 -0400

    Style App and Cell, making cell width more dynamic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { scale, width, height } from "./constants/constants";
 import "./App.scss";
+import "./index.scss";
 
 // Helper functions
 import { create2dArray } from "./utils/utils";
 
 // Component imports
 import Grid from "./components/Grid";
+import Desc from "./components/Desc";
+
+// Styled components
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 
 function App() {
   const [cells, setCells] = useState([]);
@@ -22,8 +33,10 @@ function App() {
   return (
     <div className="App">
       <h1>Conway's Game of Life</h1>
-      <Grid rows={rows} cols={cols} cells={cells} />
-      <p>Rules of the Game</p>
+      <Container>
+        <Grid rows={rows} cols={cols} cells={cells} />
+        <Desc />
+      </Container>
     </div>
   );
 }
