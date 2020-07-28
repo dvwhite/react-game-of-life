f20a32291fe9bb8a4cfde09d2c1d9b150f1f10e5
David White <dwhite0627@gmail.com>
2020-07-27 22:30:33 -0400

    Restyle button group spacing

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -6,8 +6,9 @@ import styled from "styled-components";
 const ButtonGroup = styled.div`
   display: flex;
   flex-flow: row wrap;
-  justify-content: space-evenly;
+  justify-content: space-between;
   margin-top: 2%;
+  width: 95%;
 `;
 
 export default ButtonGroup;
